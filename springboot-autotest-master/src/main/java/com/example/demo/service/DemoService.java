1b922502d2e22b7e6f42179e38261151f89f9e74
jiagou <14089970+jiagoushiluxian@user.noreply.gitee.com>
2024-03-01 15:09:08 +0800

    first commit

---
+++ springboot-autotest-master/src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+/** 
+ * @author 作者 jiagoushi: 
+ * @date 创建时间：2023年11月11日 下午5:25:00 
+ * @version 1.0 
+ * @parameter 
+ * @since 
+ * @return 
+ */
+public interface DemoService {
+	
+	public String sayHello();
+}
